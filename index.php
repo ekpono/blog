ddeb87e967ae6237f80cee6f1eb62d5abc3bb3af
ekpono <ekponoambrose@gmail.com>
2018-02-27 13:04:50 -0800

    Last commit

--- index.php
+++ index.php
@@ -14,7 +14,9 @@ div {
 <caption><h2 style="color:red">Login</h2></caption>
 <form method="POST" action="process.php">
     Name: <input type="name" name="name"><br><br>
+    Address: <input type="name" name="name"><br><br>
     Email: <input type='email' name='email'><br><br>
+    Address: <input type="address" name="address"><br><br>
     <button input type='submit' name='submit'>Submit</button>
     <button input type='reset' name='reset'>Reset</button>
 </div>
